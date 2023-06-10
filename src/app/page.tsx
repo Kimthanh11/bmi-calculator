d2cca55921fe0a8a6bf7efb313056aef3a3f8a24
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-06-10 11:25:04 +0700

    Set up content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,76 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* Start: Hero section */}
+      <p>Body Mass Index Calculator</p>
+      <p>
+        Better understand your weight in relation to your height using our body
+        mass index (BM) calculator. While BMI is not the sole determinant of a
+        healthy weight, it offers a valuable starting point to evaluate your
+        overall health and well-being.{" "}
+      </p>
+      <p>
+        Enter your details below Metric Imperial Height Weight Your BMI is...
+      </p>
+      {/* <!-- add score --> */}
+      Your BMI suggests you're .{/* <!-- add classification --> */}
+      Your ideal weight is between .{/* <!-- add range --> */}
+      {/* End: Hero section */}
+      {/* Start: Your result */}
+      <p>What your BMI result means</p>
+      <p>
+        A BMI range of 18.5 to 24.9 is considered a 'healthy weight.'
+        Maintaining a healthy weight may lower your chances of experiencing
+        health issues later on, such as obesity and type 2 diabetes. Aim for a
+        nutritious diet with reduced fat and sugar content, incorporating ample
+        fruits and vegetables. Additionally, strive for regular physical
+        activity, ideally about 30 minutes daily for five days a week.
+      </p>
+      {/* End: Your result */}
+      {/* Start: Tips */}
+      <p>Healthy eating</p>
+      Healthy eating promotes weight control, disease prevention, better
+      digestion, immunity, mental clarity, and mood.
+      <p>Regular exercise</p>
+      <p>
+        Exerciseimproves fitness, aids weight control, elevates mood, and
+        reduces disease risk, fostering wellness and longevity.
+      </p>
+      <p>Adequate sleep</p>
+      <p>
+        Sleep enhances mental clarity, emotional stability, and physical
+        wellness, promoting overall restoration and rejuvenation.
+      </p>
+      {/* Start: Tips */}
+      {/* Start:Bmi-limitations */}
+      <p>Limitations of BMI </p>
+      <p>
+        Although BMI is often a practical indicator of healthy weight, it is not
+        suited for every person. Specific groups should carefully consider their
+        BMI outcomes, and in certain cases, the measurement may not be
+        beneficial to use.
+      </p>
+      <p>Gender</p>
+      The development and body fat composition of girls and boys vary with age.
+      Consequently, a child's age and gender are considered when evaluating
+      their BMI.
+      <p>Age</p>
+      In aging individuals, increased body fat and muscle loss may cause BMI to
+      underestimate body fat content.
+      <p>Muscle</p>
+      BMI may misclassify muscular individuals as overweight or obese, as it
+      doesn't differentiate muscle from fat.
+      <p>Pregnancy</p>
+      Expectant mothers experience weight gain due to their growing baby.
+      Maintaining a healthy pre-pregnancy BMI is advisable to minimise health
+      risks for both mother and child.
+      <p>Race</p>
+      Certain health concerns may affect individuals of some Black and Asian
+      origins at lower BMIs than others. To learn more, it is advised to discuss
+      this with your GP or practice nurse.
+      {/* End:Bmi-limitations */}
     </main>
-  )
+  );
 }
